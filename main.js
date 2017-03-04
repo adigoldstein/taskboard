b7e5ac6cadfbdb1bc33adcf3f034d72757fd4683
Adi Goldstein <adij82@gmail.com>
2017-03-04 11:44:25 +0200

    create the members labels out of JSON

--- main.js
+++ main.js
@@ -1,22 +1,58 @@
-function addBtnListener(btnToListen) {
 
-  btnToListen.addEventListener('click', function (e) {
+function addNoteWTextAndLabels(notesUlElem, noteinfo) {
+  console.info(noteinfo.text);
+  const liNoteElem = document.createElement('li');
+  liNoteElem.className = 'note';
+  const editBtnElem = document.createElement('button');
+  editBtnElem.setAttribute('type', 'button');
+  editBtnElem.setAttribute('class', 'btn btn-primary btn-xs note-edit-btn pull-right');
 
+  const noteTextSpan = document.createElement('span');
+  let noteText = '';
+    if (!noteinfo) {
+      noteTextSpan.textContent = 'New note created...'
+    } else {
+      noteTextSpan.textContent = noteinfo.text
+    }
+    editBtnElem.textContent = 'Edit'
+  liNoteElem.innerHTML = noteText;
+  liNoteElem.appendChild(editBtnElem)
+  liNoteElem.appendChild(noteTextSpan)
+  const labelDivElem = document.createElement('div');
+  labelDivElem.setAttribute('class', 'lable-div');
+  liNoteElem.appendChild(labelDivElem)
+  for ( const member of noteinfo.members) {
+    labelElem = document.createElement('span');
+    labelElem.setAttribute('class', 'label label-primary pull-right');
+    labelElem.innerHTML = member;
+    labelDivElem.appendChild(labelElem)
+  }
+  // console.info(liNoteElem);
+
+  editNoteListener(liNoteElem)
+  notesUlElem.appendChild(liNoteElem);
+}
+
+
+function addCardBtnListener(btnToListen) {
+
+  btnToListen.addEventListener('click', function addCard (e) {
+console.info(e.target);
     const notesUlElem = (e.target.closest('.card').querySelector('.notes-ul'));
-    const liNoteElem = document.createElement('li');
+    addNoteWTextAndLabels(notesUlElem);
 
-    liNoteElem.className = 'note';
-    liNoteElem.innerHTML = ' <button type="button" class="btn btn-primary btn-xs pull-right" >Edit</button>New note created...';
-    notesUlElem.appendChild(liNoteElem);
+
+
+
   })
 }
 
-function addExistBtnListener() {
+function addCardExistBtn() {
 
   const addNoteBtns = document.querySelectorAll('.add-note-btn');
 
   for (const button of addNoteBtns) {
-    addBtnListener(button);
+    addCardBtnListener(button);
   }
 }
 const tampletLi = `
@@ -56,26 +92,25 @@ function addList(listData) {
 // console.info(listData);
   if (listData.type !== 'click') {
 // ******************When inserting JSON data**********************
-    console.info(listData)
+//     console.info(listData)
     const cardTitle = liListElem.querySelector('.panel-title');
     const noteUl = liListElem.querySelector('.notes-ul')
     // console.info(noteUl);
     cardTitle.innerHTML = listData.title;
-    for (const task of listData.tasks) {
-      // console.info(task.text);
-      const taskAdded = document.createElement('li');
-      taskAdded.innerHTML = task.text;
 
-      noteUl.appendChild(taskAdded)
+    for (const task of listData.tasks) {
+      console.info(task);
+     addNoteWTextAndLabels(noteUl, task)
     }
+
 
 
 
   }
 
-  // add button listener
+  // add card button listener
 
-  addBtnListener(liListElem.querySelector('.add-note-btn'));
+  addCardBtnListener(liListElem.querySelector('.add-note-btn'));
   titleListenerToRename(liListElem.querySelector('.panel-heading'));
   inputListener(liListElem.querySelector('.panel-heading'));
 
@@ -92,10 +127,15 @@ function addList(listData) {
   const deleteCardLiElem = liListElem.querySelector('.delete-card');
   deleteCardListener(deleteCardLiElem);
 
+  // Edit note listener
+
   // insert created new list before the last list
   mainUlList.insertBefore(liListElem, addListLI);
 
 }
+
+
+
 
 // Change list name
 function hideH3FocusInput(e) {
@@ -237,7 +277,7 @@ for (const noteElem of noteElems) {
 }
 // Init the app
 function init() {
-  addExistBtnListener();
+  addCardExistBtn();
 
   let panelHeadingElem = document.querySelectorAll('.content-card .panel-heading');
 
